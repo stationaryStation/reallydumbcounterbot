ba6b7c2563edc2a0fe9633386f103a57d9375214
Raúl (Station) Pérez <raulytstation@Outlook.com>
2022-11-22 09:04:56 -0400

    refactor: Prettify code

--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
-const { Client } = require("revolt.js")
+const { Client } = require("revolt.js");
 
 const client = new Client();
 
 const list = {
-  servers: [
-    "01F7ZSBSFHQ8TA81725KQCSDDP",
-    "01G2XBZ9Q16KSD41RS1NTEC3Z7"
-  ],
-  channels: [
-    "01FD5J33XHKGMJJ9XCM71B9924",
-    "01GJEBEESA2S02X6CDA5PG4NC7"
-  ]
+  servers: ["01F7ZSBSFHQ8TA81725KQCSDDP", "01G2XBZ9Q16KSD41RS1NTEC3Z7"],
+  channels: ["01FD5J33XHKGMJJ9XCM71B9924", "01GJEBEESA2S02X6CDA5PG4NC7"],
 };
-
-
 
 client.on("ready", async () => {
   console.log(`Logged in as ${client.user._id}`);
-})
+});
 
 client.on("message", async (m) => {
   const authorID = client.user._id;
@@ -26,29 +18,36 @@ client.on("message", async (m) => {
       if (m.channel.server_id === e) {
         list.channels.forEach((e) => {
           if (m.channel_id === e) {
-            const content = m.content.split(' ').filter(c => c.match(/([0-9])/g)).join();
+            const content = m.content
+              .split(" ")
+              .filter((c) => c.match(/([0-9])/g))
+              .join();
             if (content) {
               const n = parseInt(content);
               const final = n + 1;
               m.channel.sendMessage(final.toString());
             }
           }
-        })
+        });
       }
-    })
-  } 
-})
+    });
+  }
+});
 
 if (!process.env.EMAIL || !process.env.PASSWORD) {
-  console.error("Error: No email/password provided\nTry Adding EMAIL=email_here PASSWORD=password_here before the command");
+  console.error(
+    "Error: No email/password provided\nTry Adding EMAIL=email_here PASSWORD=password_here before the command"
+  );
   process.exit(1);
 }
 
-client.login({
-  "email": process.env.EMAIL,
-  "password": process.env.PASSWORD
-}).catch((e) => {
-  console.log(e)
-})
+client
+  .login({
+    email: process.env.EMAIL,
+    password: process.env.PASSWORD,
+  })
+  .catch((e) => {
+    console.log("Error while logging in", e);
+  });
 
 // We do a little bit of trolling ;p
